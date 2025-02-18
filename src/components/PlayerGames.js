14e895718c7052d5fffe88f69d36bf94774522f9
Jordan <jordan.sintes@gmail.com>
2025-02-18 13:59:41 +0100

    Modification des conditions de fin de match, clarification du code

--- src/components/PlayerGames.js
+++ src/components/PlayerGames.js
@@ -15,17 +15,19 @@ import "../styles/PlayerGames.css";
 const PlayerGames = ({ games, searchedPlayer }) => {
   if (!games || games.length === 0) return <Typography>Aucune partie trouvée.</Typography>;
 
+  const drawConditions = ["agreed", "repetition", "stalemate", "insufficient", "50move"];
+
+  //Calcul des statistiques de parties
   const calculateStats = (games) => {
     let wins = 0;
     let losses = 0;
     let draws = 0;
 
     games.forEach((game) => {
-      console.log(game.white.result);
-      if ((game.white.result === "win" && game.white.username === searchedPlayer) || 
-          (game.black.result === "win" && game.black.username === searchedPlayer)) {
+      if ((game.white.result === "win" && game.white.username.toLowerCase() === searchedPlayer) || 
+          (game.black.result === "win" && game.black.username.toLowerCase() === searchedPlayer)) {
         wins++;
-      }  else if (game.white.result !== "win" && game.black.result !== "win") {
+      }  else if (drawConditions.includes(game.white.result) && drawConditions.includes(game.black.result)) {
         draws++;
       } else{
         losses++;
@@ -35,11 +37,25 @@ const PlayerGames = ({ games, searchedPlayer }) => {
   };
 
   const { wins, losses, draws } = calculateStats(games);
+  const totalGames = wins + losses + draws;
+  const winrate = totalGames > 0 ? ((wins / totalGames) * 100).toFixed(1) : 0;
+
+  // Afficher le résultat de la partie
+  const renderPlayerResult = (player, result) => {
+    if (result === "win") {
+      return <p id="winner">{player} 👑 </p>;
+    } else if (drawConditions.includes(result)) {
+      return <p id="draw">{player} ({result})</p>;
+    } else {
+      return <p id="looser">{player}</p>;
+    }
+  };
 
   return (
     <div>
       <Typography variant="h3">Parties jouées: {games.length}</Typography>
-      <Typography>Victoire: {wins} , Défaite: {losses}, Nulle: {draws} </Typography>
+      <Typography>Victoire: {wins}, Défaite: {losses}, Nulle: {draws}</Typography>
+      <Typography>Winrate: {winrate}%</Typography>
       <TableContainer component={Paper}>
         <Table>
           <TableHead>
@@ -55,20 +71,8 @@ const PlayerGames = ({ games, searchedPlayer }) => {
             {games.map((game, index) => (
               <TableRow key={index}>
                 <TableCell>{new Date(game.end_time * 1000).toLocaleDateString()}</TableCell>
-                <TableCell>
-                  {game.white.result === "win" ? (
-                    <strong id="winner">{game.white.username} 👑 </strong>
-                  ) : (
-                    <p id="looser">{game.white.username} ({game.white.result})</p>
-                  )}
-                </TableCell>
-                <TableCell>
-                  {game.white.result === "win" ? (
-                    <p id="looser">{game.black.username} ({game.black.result})</p>
-                  ) : (
-                    <strong id="winner">{game.black.username} 👑</strong>
-                  )}
-                </TableCell>
+                <TableCell>{renderPlayerResult(game.white.username, game.white.result)}</TableCell>
+                <TableCell>{renderPlayerResult(game.black.username, game.black.result)}</TableCell>
                 <TableCell>{game.time_class}</TableCell>
                 <TableCell>
                   <a href={game.url} target="_blank" rel="noopener noreferrer">
